5517db15836ed104e61c1ac9017bf397d2430415
Tegar Machfudzi <v-tegarmachfudzi@anteraja.id>
2024-05-27 22:59:16 +0700

    first commit

---
+++ src/test/java/com/demo/kafka/consumer/demo_kafka_consumer/DemoKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.consumer.demo_kafka_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
